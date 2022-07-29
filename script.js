8a80d557b310913be4370ec31b5a2ed1db959112
PiotrPrus <pprus94@gmail.com>
2022-07-29 16:38:03 +0200

    Javascript done

--- script.js
+++ script.js
@@ -3,15 +3,9 @@ const charts = document.querySelectorAll('.chart')
 
 function openPrice() {
 
-		closePirce()
-		this.firstElementChild.classList.toggle('active')
-	
+    this.firstElementChild.classList.toggle('active')
 }
 
-const closePirce = () => {
-
-	charts.forEach(item => item.classList.remove('active'))
-}
 
 charts.forEach(chart => chart.addEventListener('click', openPrice))
 
